4c0c2773561d706fabb00416d7cd058069aa5172
soojwang <soojwang@safaricom.co.ke>
2023-11-24 18:36:22 +0300

    added quote selection functionality

--- src/pages/item.js
+++ src/pages/item.js
@@ -9,39 +9,49 @@ import { getCurrentEvent, getEvents } from '../store/slices/eventSlice';
 import ListEventsModal from '@/components/ListItemEventsModal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faTumblr } from '@fortawesome/free-brands-svg-icons';
+
+const quotes = [
+	'When I stand before God at the end of my life, I would hope that I would not have a single bit of talent left and could say, I used everything you gave me.',
+];
 
 const ItemList = () => {
+	const [activeQuote, setActiveQuote] = useState('');
+
+	const changeQuote = () => {
+		const quoteIndex = Math.round(Math.random() * 10);
+		console.log(Math.round(Math.random() * 10));
+		setActiveQuote(quotes[quoteIndex]);
+	};
+
+	useEffect(() => {
+		setActiveQuote(quotes[0]);
+	}, []);
 	return (
 		<>
 			<div
 				id='quote-box'
 				class='flex flex-col items-center justify-center h-screen'
 			>
-				<div id='quote-section'>
-					When I stand before God at the end of my life, I would hope that I
-					would not have a single bit of talent left and could say, I used
-					everything you gave me.
-				</div>
-				<div>
+				<div id='quote-section'>{activeQuote}</div>
+				<div id='author'>
 					<p> - Stephen Ochieng</p>
 				</div>
+				<br />
 				<div id='btn-section'>
-					<div id='icons-container'>
-						<a
-							href='twitter.com/intent/tweet'
-							id='tweet-quote'
-						>
-							<FontAwesomeIcon icon={faTwitter} />
-						</a>
-						<a
-							href='twitter.com/intent/tweet'
-							id='tweet-quote'
-						>
-							tweet icon
-						</a>
+					<div id='tweet-quote'>
+						<FontAwesomeIcon icon={faTwitter} />
+					</div>
+					<div id='tumblr-quote'>
+						<FontAwesomeIcon icon={faTumblr} />
 					</div>
 					<div>
-						<button id='quote-btn'>New Quote</button>
+						<button
+							id='quote-btn'
+							onClick={changeQuote}
+						>
+							New Quote
+						</button>
 					</div>
 				</div>
 			</div>
